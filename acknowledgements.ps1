c69c6e3bf5acc5a8a6123e7f845ae405c9622725
E.Z. Hart <hartez@gmail.com>
2013-01-29 21:01:47 -0700

    Attempting to verify that the CSharp assembly is loaded

--- acknowledgements.ps1
+++ acknowledgements.ps1
@@ -36,44 +36,69 @@ function Get-ScriptDirectory
   Split-Path $Invocation.MyCommand.Path
 }
 
+function Get-FrameworkDirectory()
+{
+    $([System.Runtime.InteropServices.RuntimeEnvironment]::GetRuntimeDirectory())
+}
+
 [xml]$packagesConfig = (Get-Content $packagesConfigFile)
 
-# $packages = @($packagesConfig.packages.package | select-object -index 0,2) | % {getNugetData $_} 
+$packages = @($packagesConfig.packages.package | select-object -index 0,2) | % {getNugetData $_} 
 
-# $packages[1]
+$csharpAssembly = [AppDomain]::CurrentDomain.GetAssemblies() |
+                ? { $_.FullName -match "^Microsoft.CSharp" }
+				
+If ($csharpAssembly -eq $null) {
+	$csharpSearchPath = (Get-FrameworkDirectory) 
 
- $razorAssembly = 
-            [AppDomain]::CurrentDomain.GetAssemblies() |
+	$csharpPath = Get-ChildItem -Path  $csharpSearchPath -r -i *.dll | 
+		Where-Object {$_.Name -eq "Microsoft.CSharp.dll" } | 
+		Select-Object -First 1 -ExpandProperty FullName
+		
+	If ($csharpPath -ne $null) {
+		Add-Type -Path $csharpPath
+		Write-Host "Found"
+	} Else {            
+		throw "The Microsoft.CSharp assembly must be loaded."
+	}
+}
+else {
+					Write-Host "already loaded"
+}
+
+$razorAssembly = [AppDomain]::CurrentDomain.GetAssemblies() |
                 ? { $_.FullName -match "^System.Web.Razor" }
     
-        If ($razorAssembly -eq $null) {
-            
-            $razorSearchPath = Join-Path `
-                -Path $PWD `
-                -ChildPath packages\Microsoft.AspNet.Razor.*\lib\net40\System.Web.Razor.dll
-                
-            $razorPath = Get-ChildItem -Path $razorSearchPath |
-                Select-Object -First 1 -ExpandProperty FullName
-            
-			If ($razorPath -eq $null) {
-				
-				# Attempt to get the Razor libraries from nuget
-				$packageDestination = ([string](Get-ScriptDirectory) + "\packages")
-				if(!(Test-Path $packageDestination))
-				{
-					mkdir $packageDestination
-				}
-				
-				nuget install Microsoft.AspNet.Razor /OutputDirectory $packageDestination
-			}
-			
-            If ($razorPath -ne $null) {
-                Add-Type -Path $razorPath
-            } Else {            
-                throw "The System.Web.Razor assembly must be loaded."
-            }
-        }
+If ($razorAssembly -eq $null) {
+	
+	$razorSearchPath = Join-Path `
+		-Path $PWD `
+		-ChildPath packages\Microsoft.AspNet.Razor.*\lib\net40\System.Web.Razor.dll
+		
+	$razorPath = Get-ChildItem -Path $razorSearchPath |
+		Select-Object -First 1 -ExpandProperty FullName
+	
+	If ($razorPath -eq $null) {
+		
+		# Attempt to get the Razor libraries from nuget
+		$packageDestination = ([string](Get-ScriptDirectory) + "\packages")
+		if(!(Test-Path $packageDestination))
+		{
+			mkdir $packageDestination
+		}
+		
+		nuget install Microsoft.AspNet.Razor /OutputDirectory $packageDestination
+	}
+	
+	If ($razorPath -ne $null) {
+		Add-Type -Path $razorPath
+	} Else {            
+		throw "The System.Web.Razor assembly must be loaded."
+	}
+}
 
+		
+		
 $language = New-Object `
      -TypeName System.Web.Razor.CSharpRazorCodeLanguage
 $engineHost = New-Object `
@@ -104,21 +129,37 @@ $templateCode = $codeWriter.ToString()
 
 $allcode = @"
 using System;
+using System.Text;
+using Microsoft.CSharp;
+using Microsoft.CSharp.RuntimeBinder;
 
 namespace Templates {
 
 	public abstract class TemplateBase {
-		public abstract void Execute();
-		public virtual void WriteLiteral(object value) {
-			Console.Write(value);
-		}
+		protected dynamic Model;
+        private StringBuilder _sb = new StringBuilder();
+        public abstract void Execute();
+        public virtual void Write(object value)
+        {{
+            WriteLiteral(value);
+        }}
+        public virtual void WriteLiteral(object value)
+        {{
+            _sb.Append(value);
+        }}
+        public string Render (dynamic model)
+        {{
+            Model = model;
+            Execute();
+            var res = _sb.ToString();
+            _sb.Clear();
+            return res;
+        }}
 	}
 }
 "@ + "`n" + $templateCode
 
-Write-Host $allCode
-
 Add-Type -typedefinition $allcode
 
 $templateinstance = new-object -typename Templates.Template
-$templateinstance.execute()
+$templateinstance.Render($packages)
