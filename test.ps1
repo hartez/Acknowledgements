39c2880bbe86f5599161c7b67b7d475ae343c9f8
E.Z. Hart <hartez@gmail.com>
2013-01-30 17:37:04 -0700

    Removed test script

--- test.ps1
+++
@@ -1,2 +0,0 @@
-$p = Split-Path $MyInvocation.MyCommand.Path
-& "$p\acknowledgements.ps1" "c:\users\hartez\documents\traceur\lhp\beadmin\lhptadminconsole\packages.config" "$p\default.cshtml" "test.html"
